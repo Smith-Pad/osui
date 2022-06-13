a461de93af77ef61b37a864431a97099b5497e18
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-13 11:21:32 -0400

    Updated file: [merge.sh]

--- merge.sh
+++ merge.sh
@@ -13,3 +13,16 @@ git checkout Smith-Pad-OS-Shell
 rm -rf .git/
 
 cp -R * ../
+
+cd ..
+
+rm -rf Smith-Pad
+
+
+git add * 
+
+
+git commit -m "Merged from [https://github.com/Smith-Pad/Smith-Pad/tree/Smith-Pad-OS-Shell"
+
+
+git push origin main
