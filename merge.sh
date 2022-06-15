edd7bc7947536a92f1c3f3182d15882366eff91a
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-15 17:59:35 -0400

    Allow merge.sh the ability to merge the [External-Apps-Launcher] repository from [https://github.com/Smith-Pad/Smith-Pad/External-Apps-Launcher]

--- merge.sh
+++ merge.sh
@@ -27,3 +27,27 @@ git commit -m "Merged from [https://github.com/Smith-Pad/Smith-Pad/tree/Smith-Pa
 
 
 git push origin main
+
+
+
+
+
+
+## Merge External-Apps-Launcher from 
+## [https://github.com/Smith-Pad/External-Apps-Launcher]
+
+## Prevent Conflicts
+rm -rf External-Apps-Launcher
+git rm External-Apps-Launcher
+git commit -m "Updating...."
+git push origin main
+
+
+
+## Merge the repository
+git clone https://github.com/Smith-Pad/External-Apps-Launcher
+cd External-Apps-Launcher
+rm -rf .git/
+cd ..
+git add External-Apps-Launcher
+git commit -m "Merged from [https://www.github.com/Smith-Pad/External-Apps-Launcher]"
