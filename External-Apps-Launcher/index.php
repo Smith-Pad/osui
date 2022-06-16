854f490d2a3289a5fbd97e129e4b8d437b5dd0b6
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-16 08:24:52 -0400

    Merged from [https://www.github.com/Smith-Pad/External-Apps-Launcher]

--- External-Apps-Launcher/index.php
+++ External-Apps-Launcher/index.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="FOIL-UI.css" type="text/css">
 
 
-
+<div class="FOIL-Navigation-Bar">
 <h1> Test Menu </h1> 
-
+</div>
 
 <a href="test-1.php" class="FOIL-button">test1</a>
 <a href="test-2.php"class="FOIL-button">test2</a>
