d9b8e9ecf086fc6e8a4362c44e89a8fdd1d03f31
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-27 15:01:28 -0400

    Merged from [https://github.com/Smith-Pad/Smith-Pad/tree/Smith-Pad-OS-Shell

---
+++ EXTERNAL_ROUTE_APPS/generate-launcher.sh
@@ -0,0 +1,13 @@
+#########################################################
+##              generate-launcher.sh
+##
+#########################################################
+
+echo '''
+<?php
+exec("TYPE HERE");
+?>
+
+<meta http-equiv="refresh" content="0; url=../applications-menu.php">
+
+''' >> NEW.php
