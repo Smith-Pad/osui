2333e5b9166344c15310cfd6636798ec74436ad1
senalbulumulle <senal.bulumulle@gmail.com>
2023-01-26 15:08:04 -0500

    Removed For loop for adjusting layouts in Home Screen [System_Files/system.global.include.content.php]

--- System_Files/system.global.include.content.php
+++ System_Files/system.global.include.content.php
@@ -1,19 +1,7 @@
 <?php
 function content() {
         
-        /** 
-         * For-loop. 
-         * 
-         * 
-         * 
-         * How to adjust the number of layouts 
-         * 
-         * On the [$layout <= NUMBER OF LAYOUTS], that's where you provide 
-         * the number of layouts to adjust.
-         */
-        for ($layout = 0; $layout <= 2; $layout +=5) {
-                print('<div class="FOIL-layout-regular-no-shadow">');   
-        } 
+        print('<div class="FOIL-layout-regular-no-shadow">');
 
         print('<center> <FOIL-font-size-60> <div id="time_service"></div> </FOIL-font-size-60> </center>');
         print('<script src="src/FOIL-time.js"></script>');
