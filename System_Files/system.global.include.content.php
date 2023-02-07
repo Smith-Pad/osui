8fe69bb32ea19c3622d16c02bd5cbd1a5ff3f3b1
senalbulumulle <senal.bulumulle@gmail.com>
2023-02-07 11:06:20 -0500

    Created Applications View in [Views/Applications-Menu]

--- System_Files/system.global.include.content.php
+++ System_Files/system.global.include.content.php
@@ -8,7 +8,7 @@ function content() {
         print('<center><FOIL-font-size-40> Menu </FOIL-font-size-40></center>');
         print('<center>');
         print('<a href="#" class="FOIL-button-regular-no-shadow">Settings</a>');
-        print('<a href="#" class="FOIL-button-regular-no-shadow">Apps</a>');
+        print('<a href="./Views/Applications-Menu/index.php" class="FOIL-button-regular-no-shadow">Apps</a>');
         print('<a href="./Views/Subjects/index.php" class="FOIL-button-regular-no-shadow">Subjects</a>');
         print("</center>");
 }
