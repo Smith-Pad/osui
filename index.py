9b03875bf5661a92e034d86947116a890437cc99
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-05 07:50:30 -0500

    Created the Home-Screen Section in index.py

--- index.py
+++ index.py
@@ -4,12 +4,30 @@ import speech_recognition as sr
 app = Flask(__name__)
 
 """ 
-
+██   ██  ██████  ███    ███ ███████     ███████  ██████ ██████  ███████ ███████ ███    ██ 
+██   ██ ██    ██ ████  ████ ██          ██      ██      ██   ██ ██      ██      ████   ██ 
+███████ ██    ██ ██ ████ ██ █████       ███████ ██      ██████  █████   █████   ██ ██  ██ 
+██   ██ ██    ██ ██  ██  ██ ██               ██ ██      ██   ██ ██      ██      ██  ██ ██ 
+██   ██  ██████  ██      ██ ███████     ███████  ██████ ██   ██ ███████ ███████ ██   ████ 
+                                                                                          
+Home-Screen                                                                                       
 
 """
+
+
 @app.route('/', methods=['POST', 'GET'])
 def home_screen():
-        return render_template('index.html')
+    
+    ## Here we name the buttons that are on the homescreen
+    home_screen_button_one   = 'Apps'
+    home_screen_button_two   = 'Settings'
+    home_screen_button_three = 'Subjects'
+    
+    return render_template('index.html',
+                           home_screen_button_one=home_screen_button_one,
+                           home_screen_button_two=home_screen_button_two,
+                           home_screen_button_three=home_screen_button_three,)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
