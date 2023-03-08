5fa98ac0b501322d043ebc79101a0a3eaaa2b7f9
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-08 10:51:29 -0500

    Yeah... will clean it out later...

--- index.py
+++ index.py
@@ -6,23 +6,9 @@ import sh
 
 app = Flask(__name__)
 
-"""
-██   ██  ██████  ███    ███ ███████     ███████  ██████ ██████  ███████ ███████ ███    ██ 
-██   ██ ██    ██ ████  ████ ██          ██      ██      ██   ██ ██      ██      ████   ██ 
-███████ ██    ██ ██ ████ ██ █████       ███████ ██      ██████  █████   █████   ██ ██  ██ 
-██   ██ ██    ██ ██  ██  ██ ██               ██ ██      ██   ██ ██      ██      ██  ██ ██ 
-██   ██  ██████  ██      ██ ███████     ███████  ██████ ██   ██ ███████ ███████ ██   ████ 
-                                                                                          
-
-Search: Home-Screen
-------------------------
-"""
-
 
 @app.route('/', methods=['POST', 'GET'])
 def home_screen():
-
-    # Here we name the buttons that are on the homescreen
     apps_button = 'Apps'
     settings_button = 'Settings'
     subjects_button = 'Subjects'
@@ -33,30 +19,8 @@ def home_screen():
                            subjects_button=subjects_button,)
 
 
-"""
-███████     ███████     ████████     ████████     ██     ███    ██      ██████      ███████
-██          ██             ██           ██        ██     ████   ██     ██           ██
-███████     █████          ██           ██        ██     ██ ██  ██     ██   ███     ███████
-     ██     ██             ██           ██        ██     ██  ██ ██     ██    ██          ██
-███████     ███████        ██           ██        ██     ██   ████      ██████      ███████
-
-
-Search: Settings Screen
-------------------------
-
-- Theme Settings
-
-- Software Update
-
-"""
-
-
-# Search: Settings Screen Main Menu
-
 @app.route('/settings_screen', methods=['POST', 'GET'])
 def settings_screen():
-
-    # Here we name the buttons that are on the homescreen
     themes_button = 'Themes'
     software_update_button = 'Software Update'
     about_system_button = 'About System'
@@ -67,31 +31,22 @@ def settings_screen():
                            about_system_button=about_system_button)
 
 
-# Themes Settings Screen
-# This allows the ability to change themes from the interface itself.
+
 @app.route('/settings_screen_themes_screen', methods=['POST', 'GET'])
 def settings_screen_themes_screen():
     return render_template('settings-theme-settings.html')
 
 
-# Software Update Screen 
-# This allows the ability to manage the OS from the interface itself.
+
 @app.route('/settings_screen_software_update_screen', methods=['POST', 'GET'])
 def settings_screen_software_update_screen():
         return render_template('settings-software-update.html')
 
-#####################################
-## Software Update Backend.
-#####################################
+
 @app.route("/software_update_service")
 def software_update_service():
         software_update_service_output = sh.pkexec.pacman("-Sy")
         return render_template('settings-software-update.html', software_update_service_output=software_update_service_output)
-
-
-
-
-
 
 
 
