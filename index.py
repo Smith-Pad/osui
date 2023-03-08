ca6ff6757d552ff2b3fcc4f0029e3bdd423bd803
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-08 14:09:37 -0500

    commented everything...

--- index.py
+++ index.py
@@ -13,10 +13,7 @@ def home_screen():
     settings_button = 'Settings'
     subjects_button = 'Subjects'
 
-    return render_template('index.html',
-                           apps_button=apps_button,
-                           settings_button=settings_button,
-                           subjects_button=subjects_button,)
+    return render_template('index.html',apps_button=apps_button, settings_button=settings_button, subjects_button=subjects_button,)
 
 
 @app.route('/settings_screen', methods=['POST', 'GET'])
@@ -25,10 +22,7 @@ def settings_screen():
     software_update_button = 'Software Update'
     about_system_button = 'About System'
 
-    return render_template('settings-main.html',
-                           themes_button=themes_button,
-                           software_update_button=software_update_button,
-                           about_system_button=about_system_button)
+    return render_template('settings-main.html', themes_button=themes_button, software_update_button=software_update_button, about_system_button=about_system_button)
 
 
 
@@ -43,14 +37,20 @@ def settings_screen_software_update_screen():
         return render_template('settings-software-update.html')
 
 
+
 @app.route("/software_update_service")
 def software_update_service():
+        ## This is where in the backend, the pacman package manager is used to update packages.
         software_update_service_output = sh.pkexec.pacman("-Sy")
+
+        
+        ## This is where the logs will be rendered to. 
         return render_template('settings-software-update.html', software_update_service_output=software_update_service_output)
+        
+        
 
-
-
-
+        
+       
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
