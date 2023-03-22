b283338e3faaa611ddf738d74fd60e0fd8be641b
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-22 09:46:14 -0400

    Cleanup

--- apps-screen-volume-rocker-up.py
+++
@@ -1,4 +0,0 @@
-@app.route('/apps_screen_volume_rocker_up', methods=['POST'])
-def apps_screen_volume_rocker_up():
-    os.system('pactl set-sink-volume @DEFAULT_SINK@ +10%')
-    return apps_screen()
--- home-screen-volume-rocker-up.py
+++
@@ -1,4 +0,0 @@
-@app.route('/home_screen_volume_rocker_down', methods=['POST'])
-def home_screen_volume_rocker_down():
-    os.system('pactl set-sink-volume @DEFAULT_SINK@ -10%')
-    return home_screen()
