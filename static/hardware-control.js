56b189564c1d72d04c80951a86c01249da9fc676
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-26 11:22:47 -0700

    Added file: [static/hardware-control.js]

---
+++ static/hardware-control.js
@@ -0,0 +1,11 @@
+/********************************************************************************
+ * hardware-control.js
+ * 
+ * This file is acts as a controller to both HTML and Flask to control
+ * hardware devices, while the view stays constantly. Say the user opens
+ * a FOIL-bar popup menu, and the user clicks on a functional button, then
+ * by default the interface refreshes and the div goes away. So this controller 
+ * prevents that happening
+ ********************************************************************************/
+
+
