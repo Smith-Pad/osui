b7d5238becfb42823184557ed68d683a0f672e3c
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-27 10:52:24 -0400

    Added more comments on [static/hardware-control.js]

--- static/hardware-control.js
+++ static/hardware-control.js
@@ -14,23 +14,52 @@
 // "Hey, I want to increase, decrease or completely mute the audio volume"
 
 function VOLUME_CONTROLLER() {
+
+
+        // Here, we create a function called volumeUp which essentially is used 
+        // when the user clicks on a button that has functionality to increase the 
+        // audio volume. 
+
         function volumeUp() {
                 document.getElementById('volume-up').addEventListener('click', function () {
+                        // Here we make a variable called requestVolumeUp,
+                        // which uses `XMLHttpRequest' which is used for initialization
                         var requestVolumeUp  = new XMLHttpRequest();
+
+                        // Here, we use the open parameter to say to python flask, 
+                        // "Hey, I want you to use the volume_rocker_up function
+                        // and increase the volume". 
                         requestVolumeUp.open('POST', '/volume_rocker_up');
+ 
+                        // Here, we use the send parameter to send the request. 
                         requestVolumeUp.send();
                 });
         }
-        volumeUp();
 
+
+        
+        // Here, we create a function called volumeDown, which essentially is used 
+        // when the user clicks on a button that has functionality to increase the 
+        // audio volume. 
 
         function volumeDown() {
                 document.getElementById('volume-down').addEventListener('click', function () {
+                        // Here we make a variable called requestVolumeDown,
+                        // which uses `XMLHttpRequest' which is used for initialization
                         var requestVolumeDown  = new XMLHttpRequest();
+
+                        // Here, we use the open parameter to say to python flask, 
+                        // "Hey, I want you to use the volume_rocker_down function
+                        // and decrease the volume". 
                         requestVolumeDown.open('POST', '/volume_rocker_down');
+
+                        // Here, we use a send parameter to send the request
                         requestVolumeDown.send();
                 });
         }
+
+
+        volumeUp();
         volumeDown();
         
 
