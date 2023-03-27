855dfcd0612a29bc078b95a39610f55d35b4e49c
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-27 09:30:01 -0400

    Removed [__SEPARATE_MODULES] folder

--- __SEPARATE_MODULES/one_module.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Response, render_template, Markup, request, jsonify
-import speech_recognition as sr
-import sh
-import os
-import subprocess
-
-def get_output():
-    output = "hello world"
-    return output
