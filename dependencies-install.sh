97441c03d0e524705b49e357235efbc6241893aa
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-29 08:49:38 -0400

    Added file: [dependencies-install.sh]

---
+++ dependencies-install.sh
@@ -0,0 +1,24 @@
+#############################################################
+#               dependencies-install.sh
+#############################################################
+
+
+## Installs Flask 
+
+pip install flask 
+
+## Installs opencv-python 
+pip install opencv-python 
+
+## Installs sh 
+pip install sh 
+
+
+## Installs mediapipe 
+pip install mediapipe 
+
+## Installs SpeechRecognition
+pip install SpeechRecognition
+
+## Installs python-xlib
+pip install python-xlib
