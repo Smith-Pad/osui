bae9d53b4bc254b7ffe5788dec62cd96e84b1b51
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-29 10:16:10 -0400

    Backup [static/hardware-control.js]

---
+++ static/hardware-control copy.js
@@ -0,0 +1,87 @@
+/********************************************************************************
+ * hardware-control.js
+ * 
+ * This file is acts as a controller to both HTML and Flask to control
+ * hardware devices, while the view stays constantly. Say the user opens
+ * a FOIL-bar popup menu, and the user clicks on a functional button, then
+ * by default the interface refreshes and the div goes away. So this controller 
+ * prevents that happening
+ ********************************************************************************/
+
+
+
+function VOLUME_CONTROLLER() {
+
+        function volumeUp() {
+                document.getElementById('volume-up').addEventListener('click', function () {
+                        var requestVolumeUp  = new XMLHttpRequest();
+
+                        /**
+                         * See the index.py
+                         */
+                        requestVolumeUp.open('POST', '/volume_rocker_up');
+                        requestVolumeUp.send();
+                });
+        }
+
+
+
+
+
+        
+
+        function volumeDown() {
+                document.getElementById('controls-volume-down').addEventListener('click', function () {
+                        var requestVolumeDown  = new XMLHttpRequest();
+
+                        /**
+                         * See the index.py
+                         */
+                        requestVolumeDown.open('POST', '/volume_rocker_down');
+
+                        requestVolumeDown.send();
+                });
+        }
+
+
+
+        
+
+        function controlsvolumeUp() {
+                document.getElementById('controls-volume-up').addEventListener('click', function () {
+                        var requestControlsVolumeUp  = new XMLHttpRequest();
+
+                        /**
+                         * See the index.py
+                         */
+                        requestControlsVolumeUp.open('POST', '/controls_volume_rocker_up');
+                        requestControlsVolumeUp.send();
+                });
+        }
+
+
+
+
+
+        function controlsvolumeDown() {
+                document.getElementById('controls-volume-down').addEventListener('click', function () {
+                        var requestControlsVolumeDown  = new XMLHttpRequest();
+
+                        /**
+                         * See the index.py
+                         */
+                        requestControlsVolumeDown.open('POST', '/controls_volume_rocker_down');
+
+                        requestControlsVolumeDown.send();
+                });
+        }
+
+
+        volumeUp();
+        controlsvolumeUp();
+        volumeDown();
+        controlsvolumeDown();
+
+}
+
+VOLUME_CONTROLLER();
