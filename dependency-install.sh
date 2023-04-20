de6520da910b01cb16ce9dbe08b3bd6fd6fcc445
senalbulumulle <senal.bulumulle@gmail.com>
2023-04-20 15:09:35 -0400

    Added file: [dependency-install.sh]

---
+++ dependency-install.sh
@@ -0,0 +1,56 @@
+## Dependency Install
+#
+#
+#
+#  Description: This script allows the ability to install dependencies for running and debugging
+#  		OSUI. 
+#
+#  2023 Smith-Pad
+#  
+###############################################################################################
+
+
+## 
+# @doc @function 
+# ---------------
+# In this function, this function will use the if statements and use the cat 
+# command to the /etc directory to the os-release file and uses the grep 
+# command to detect what distribution that the system is running
+# 
+#
+# For instance, if your are running let's say, Smith-Pad-OS, then it will
+# install dependencies in order to run and debug OSUI
+#
+#
+# @doc @function
+# ---------------
+# The package_manager<distro name> function represents the commands to install
+# dependencies via a distro's default package manager.
+#
+# The package_manager<distro name>_python_pip_package_manager<distro name> function 
+# represents the commands to install dependencies via the Python pip package manager
+#  
+#
+#
+
+function check() {
+        if cat /etc/os-release|grep openSUSE &> /dev/null; then 
+		package_manager_openSUSE
+		package_manager_openSUSE_python_pip_package_manager
+	fi
+}
+
+
+
+function package_manager_openSUSE() {
+	sudo zypper install python310-pip
+	sudo zypper install php8-cli
+}
+
+function package_manager_openSUSE_python_pip_package_manager() {
+	pip3.10 install flask
+	pip3.10 install speechrecognition
+	pip3.10 install sh
+}
+
+check
