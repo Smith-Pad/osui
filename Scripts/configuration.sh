a0f50c2fde1850a550b4e111986e8528e2b88b3e
senalbulumulle <senal.bulumulle@gmail.com>
2023-05-13 02:25:02 +0530

    mmm

--- Scripts/configuration.sh
+++ Scripts/configuration.sh
@@ -1,10 +1,16 @@
 ## configuration.sh
 
+####################################################################
 ## Functions
-
+####################################################################
 ## 1. xinitrc   configuration -> root -> /root/.config/autostart 
 ## 2. xsessions configuration -> root -> /usr/share/xsessions/
+## 3. xsessions script        -> root -> /usr/bin/osui-launch.sh
+####################################################################
 
+################
+#       1      #
+################
 
 if [ ! -d "/root/.config/autostart" ]; then
         mkdir -p /root/.config/autostart
@@ -13,15 +19,22 @@ else
         echo "Directory /root/.config/autostart already exists"
 fi
 
-echo """
+
+
+sudo su <<EOF
+
+echo '
 [Desktop Entry]
 Name=osui-desktop
 Type=Application
 Exec=firefox --kiosk http://127.0.0.1:3000
+' > /root/.config/autostart/osui.desktop
 
-""" >> /root/.config/autostart/osui.desktop
+EOF
 
-
+################
+#       2      #
+################
 
 
 sudo su <<EOF
@@ -37,5 +50,29 @@ Type=Application
 X-LightDM-DesktopName=OSUI
 DesktopNames=OSUI
 ' > /usr/share/xsessions/
+
+EOF
+
+
+
+
+
+
+################
+#       3      #
+################
+
+
+sudo su <<EOF
+
+
+echo '
+## OSUI launch script
+## Description: This script is used when using the xsessions and display manager
+
+kwin_x11 --replace &
+
+firefox --kiosk 127.0.0.1:3000
+' > /usr/bin/osui-launch.sh
 
 EOF
