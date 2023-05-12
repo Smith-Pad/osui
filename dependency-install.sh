a70fbe728c3119250bd735c95336b30713fa7155
senalbulumulle <senal.bulumulle@gmail.com>
2023-05-12 08:00:56 -1000

    mmm

--- dependency-install.sh
+++ dependency-install.sh
@@ -71,6 +71,8 @@ function package_manager_Arch() {
         sudo pacman -S python-pyaudio --noconfirm
         sudo pacman -S xorg-server --noconfirm
         sudo pacman -S xorg-xinit --noconfirm
+        sudo pacman -S wayland --noconfirm
+        sudo pacman -S wayfire --noconfirm
 }
 function package_manager_Arch_python_pip_package_manager() {
         pip install flask
