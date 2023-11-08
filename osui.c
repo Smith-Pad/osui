e576f0878b0a2cee522932269218b28235ec53ab
senalbulumulle <senal.bulumulle@gmail.com>
2023-11-07 19:27:31 -0500

    Included global header libraries

---
+++ osui.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys.stat.h>
+#include <immintrin.h>
