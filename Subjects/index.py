7d9f352b608732686e47a99be93271817f819181
senalbulumulle <senal.bulumulle@gmail.com>
2023-11-13 11:36:36 -0500

    Added/routed file: [Subjects/templates/main_menu]

--- Subjects/index.py
+++ Subjects/index.py
@@ -3,3 +3,19 @@ import os
 
 
 app  = Flask(__name__)
+
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/main_menu')
+def main_menu():
+    return render_template('main_menu.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
