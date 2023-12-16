b1de769fa1d06675a307b279ef473e634ab27d06
senalbulumulle <senal.bulumulle@gmail.com>
2023-12-16 01:50:56 +0000

    Make the mockup install script create a directory and clone and checkout mockup-html branch in the home directory

--- mockup-dependency-install.sh
+++ mockup-dependency-install.sh
@@ -6,3 +6,8 @@ sudo pacman -S xorg --noconfirm
 sudo pacman -S xorg-server --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
 sudo pacman -S chromium --noconfirm
+
+
+mkdir osui-mockup && cd osui-mockup
+git clone https://github.com/smith-pad/osui . 
+git checkout osui-mockup
