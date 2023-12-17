dc51b780ae7be1bacf2193e5ebdc4a026e46e6fa
senalbulumulle <senal.bulumulle@gmail.com>
2023-12-17 19:33:22 +0000

    Just to make sure.... add xfce as a package in the mockup-dependency-install script

--- mockup-dependency-install.sh
+++ mockup-dependency-install.sh
@@ -2,6 +2,7 @@
 
 sudo pacman -Syyu --noconfirm
 sudo pacman -S xfwm4 --noconfirm
+sudo pacman -S xfce4 --noconfirm
 sudo pacman -S xorg --noconfirm
 sudo pacman -S xorg-server --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
