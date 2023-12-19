b284985ef50bde050db41b81725cd504aff8edf0
senalbulumulle <senal.bulumulle@gmail.com>
2023-12-19 16:27:44 +0000

    Updated [mockup-dependency-install-lxde-based.sh]

--- mockup-dependency-install-lxde-based.sh
+++ mockup-dependency-install-lxde-based.sh
@@ -1,1 +1,16 @@
 ## mockup-dependency-install-lxde-based.sh
+
+sudo pacman -Syyu --noconfirm
+
+
+## Remove the .xinitrc if necessary if it is existed
+cd $HOME
+rm -rf .xinitrc
+
+
+## Remove the autostart file if it is existed
+cd $HOME/.config
+rm -rf autostart/osui-launch.desktop
+
+## Remove the xfce4 dependencies if necessary
+sudo pacman -Rcns xfce4 --noconfirm
