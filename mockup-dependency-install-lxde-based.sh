7d1c9ae33f16a14b3ecf95710d9f541798d73904
senalbulumulle <senal.bulumulle@gmail.com>
2023-12-20 14:52:48 +0000

    Updated [mockup-dependency-install-lxde-based.sh]

--- mockup-dependency-install-lxde-based.sh
+++ mockup-dependency-install-lxde-based.sh
@@ -11,24 +11,3 @@ rm -rf .xinitrc
 ## Remove the autostart file if it is existed
 cd $HOME/.config
 rm -rf autostart/osui-launch.desktop
-
-# ## Remove the xfce4 dependencies if necessary
-# sudo pacman -Rcns xfce4 --noconfirm
-
-
-# ## Then install all of the LXDE dependencies
-# sudo pacman -S lxde --noconfirm
-
-# ## Then uninstall not really necessary LXDE dependencies
-# sudo pacman -Rcns lxpanel --noconfirm
-
-# ## Install the .xinitrc config 
-# cd $HOME
-# touch .xinitrc
-# echo """
-# exec lxsession
-
-# """ >> .xinitrc
-
-# ## cat out the .xinitrc file
-# cat $HOME/.xinitrc
