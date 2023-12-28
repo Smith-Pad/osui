788b823399d4897465a1ff92432fb2115377ac23
senalbulumulle <senal.bulumulle@gmail.com>
2023-12-28 00:32:28 -0500

    this may work...

--- mockup-dependency-install-lxde-based-rewritten-another.sh
+++ mockup-dependency-install-lxde-based-rewritten-another.sh
@@ -16,6 +16,28 @@ function getPackages() {
     sudo pacman -S xorg-xinit --noconfirm
     sudo pacman -S xorg-xrandr --noconfirm
     sudo pacman -S kwin --noconfirm
+    sudo pacman -S chromium --noconfirm
 }
 
+function configureStuff() {
+    cp -R systemd $HOME/.config/                    ## Install the systemd configurations
+    cp -R php-mockup-osui-service.sh $HOME          ## Install the sh php scripts
+    cp -R lxsession $HOME/.config                   ## Install the lxde configuration
+    cp -R .xinitrc $HOME                            ## Install the .xinitrc configuration
+    
+    ## This is where it goes to the default user home directory, 
+    ## and then creates a directory called osui-mockup, and 
+    ## then git gets the ability to clone and checkout to the 
+    ## "mockup-html" branch, which is used and the source 
+    ## to the OSUI mockup version. 
+    
+    cd $HOME
+    mkdir osui-mockup
+    git clone https://github.com/smith-pad/osui .  
+}
+
+
+
 upgradePackages
+getPackages
+configureStuff
