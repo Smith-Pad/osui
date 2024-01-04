036d27e2212c1cb4ee27c67753e5f4f350e6e52d
senalbulumulle <senal.bulumulle@gmail.com>
2024-01-04 14:50:38 -0500

    Added an sample lesson... [Used for development purposes]

--- Subjects/index.py
+++ Subjects/index.py
@@ -17,5 +17,22 @@ def main_menu():
     return render_template('main_menu.html')
 
 
+@app.route('/sample_lesson')
+def sample_lesson():
+    return render_template('sample-lesson/index.html')
+
+
+@app.route('/sample_lesson__main_menu')
+def sample_lesson__main_menu():
+    return render_template('sample-lesson/game_menu.html')
+
+
+@app.route('/sample_lesson__introduction')
+def sample_lesson__introduction():
+    return render_template('sample-lesson/introduction.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
