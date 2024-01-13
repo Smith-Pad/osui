901955500619b73e7967312601237c2635b73c96
senalbulumulle <senal.bulumulle@gmail.com>
2024-01-13 13:02:15 -0500

    mmm

--- dependency-install.sh
+++ dependency-install.sh
@@ -1,7 +1,20 @@
 ## dependency-install.sh
 
+
+sudo pacman -Syyu --noconfirm
 sudo pacman -S xfwm4 --noconfirm
+sudo pacman -S xfce4 --noconfirm
 sudo pacman -S xorg --noconfirm
 sudo pacman -S xorg-server --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
-sudo pacman -S chromium --noconfirm     ## This is temporary
+sudo pacman -S chromium --noconfirm
+sudo pacman -S polybar --noconfirm
+
+## Remove some dependencies of xfce4
+sudo pacman -Rcns xfce4-appfinder --noconfirm
+sudo pacman -Rcns xfdesktop --noconfirm
+sudo pacman -Rcns xfce4-power-manager --noconfirm
+sudo pacman -Rcns tumbler --noconfirm
+sudo pacman -Rcns thunar-volman --noconfirm
+sudo pacman -Rcns garcon --noconfirm
+sudo pacman -Rcns exo --noconfirm
