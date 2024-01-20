51a2c95a4dc4dab071fde7420e044d7afbe60dab
senalbulumulle <senal.bulumulle@gmail.com>
2024-01-19 23:06:32 -0500

    Added a [development-environment-setup.sh]

---
+++ development-environment-setup.sh
@@ -0,0 +1,29 @@
+## development-environment-setup.sh
+## This script allows the ability to set up the development environment for 
+## developing osui environment on Arch Linux or Smith-Pad-OS
+
+
+## The first thing to do is to update the packages
+function updatePackages() {
+    sudo pacman -Syyu --noconfirm
+}
+
+
+## Install the editors
+function editors(){
+    sudo pacman -S vim --noconfirm
+    sudo pacman -S nano --noconfirm
+    sudo pacman -S micro --noconfirm
+}
+
+
+## Install the desktop environment xfce4 for the development environment
+function desktopEnvironment() {
+    sudo pacman -S xfce4 --noconfirm 
+}
+
+
+
+updatePackages
+editors
+desktopEnvironment
