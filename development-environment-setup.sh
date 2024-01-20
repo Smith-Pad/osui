92ae288a6b617e1836d3e84512ed3d7fe25e421c
senalbulumulle <senal.bulumulle@gmail.com>
2024-01-20 14:14:38 -0500

    Removed the functions

--- development-environment-setup.sh
+++ development-environment-setup.sh
@@ -4,41 +4,34 @@
 
 
 ## The first thing to do is to update the packages
-function updatePackages() {
-    sudo pacman -Syyu --noconfirm
-}
+
+sudo pacman -Syyu --noconfirm
+
 
 
 ## Install the editors
-function editors(){
-    sudo pacman -S vim --noconfirm
-    sudo pacman -S nano --noconfirm
-    sudo pacman -S micro --noconfirm
-}
+sudo pacman -S vim --noconfirm
+sudo pacman -S nano --noconfirm
+sudo pacman -S micro --noconfirm
+
 
 
 ## Install the desktop environment xfce4 for the development environment
-function desktopEnvironment() {
-    sudo pacman -S xfce4 --noconfirm 
-}
+
+sudo pacman -S xfce4 --noconfirm 
+
 
 
 ## Install the display manager for xfce4 for the development environment
-function displayManager() {
-    sudo pacman -Rcns lightdm --noconfirm
-    sudo systemctl stop lightdm
-    sudo systemctl disable lightdm
-    sudo pacman -S sddm --noconfirm
-    sudo systemctl enable sddm 
-    sudo systemctl start sddm
-}
+
+sudo pacman -Rcns lightdm --noconfirm
+sudo systemctl stop lightdm
+sudo systemctl disable lightdm
+sudo pacman -S sddm --noconfirm
+sudo systemctl enable sddm 
+sudo systemctl start sddm
+
 
 
 ## Reboot the system 
 sudo reboot
-
-
-updatePackages
-editors
-desktopEnvironment
-displayManager
