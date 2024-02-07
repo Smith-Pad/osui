74106574fd2457d5d1f85e860869658157c0d49d
senalbulumulle <senal.bulumulle@gmail.com>
2024-02-07 02:40:59 -0500

    Added the [development-environment-setup-i3-setup-uninstall.sh]

---
+++ development-environment-setup-i3-setup-uninstall.sh
@@ -0,0 +1,25 @@
+## development-environment-setup.sh
+## This script allows the ability to set up the development environment for 
+## developing osui environment on Arch Linux or Smith-Pad-OS
+
+## This version of this script installs the i3 window manager for ease of use, 
+## for preference. 
+
+
+## The first thing to do is to update the packages
+
+sudo pacman -Syyu --noconfirm
+
+
+
+
+## Uninstall the editors
+sudo pacman -Rcns vim --noconfirm
+sudo pacman -Rcns nano --noconfirm
+sudo pacman -Rcns micro --noconfirm
+
+
+
+
+## Uninstall the desktop environment i3
+sudo pacman -Rcns i3 --noconfirm 
