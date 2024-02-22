a52806e559546b72e2ce3089c415d12ac69f32f5
senalbulumulle <senal.bulumulle@gmail.com>
2024-02-22 10:07:00 -0500

    Removed the functions...

--- distribution-scripts/install-dependencies-opensuse.sh
+++ distribution-scripts/install-dependencies-opensuse.sh
@@ -2,37 +2,20 @@
 
 ## This script is used to install dependencies to run OSUI.
 
+ 
 
-function updatePackages() 
-{
-	sudo zypper update
-}
-
-
-function uninstallPackages() 
-{
-	sudo zypper remove xfce4-appfinder 	
-	sudo zypper remove xfce4-appfinder-lang
-	sudo zypper remove xfdesktop
-	sudo zypper remove xfce4-panel
-}
-
-
-
-function installPackages()
-{
-	sudo zypper install git-core
-	sudo zypper install xorg-scripts
-	sudo zypper install chromium
-}
+sudo zypper update
 
 
 
 
+sudo zypper remove xfce4-appfinder 	
+sudo zypper remove xfce4-appfinder-lang
+sudo zypper remove xfdesktop
+sudo zypper remove xfce4-panel
 
 
 
-
-updatePackages
-uninstallPackages
-installPackages
+sudo zypper install git-core
+sudo zypper install xorg-scripts
+sudo zypper install chromium
