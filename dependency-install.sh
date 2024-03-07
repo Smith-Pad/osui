6232f2ae6b210270bcedaee692a2f6f502d1e6a1
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-07 14:42:00 +0000

    Make the script install flask via pacman, instead of pip for the arch linux dependency install...

--- dependency-install.sh
+++ dependency-install.sh
@@ -72,6 +72,7 @@ function package_manager_Arch() {
         sudo pacman -S xorg-server --noconfirm
         sudo pacman -S xorg-xinit --noconfirm
         sudo pacman -S wayland --noconfirm
+        sudo pacman -S python-flask --noconfirm
 }
 # function package_manager_Arch_python_pip_package_manager() {
 #         pip install flask
