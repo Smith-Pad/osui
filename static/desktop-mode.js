084cd81830bd0797f0eee4e490df8c66abb38529
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-08 20:16:09 -0400

    oh...

--- static/desktop-mode.js
+++ static/desktop-mode.js
@@ -1,13 +1,13 @@
 /********************************************************************************
  * desktop-mode.js
- * Desktop Mode allows the user to experience the GNOME Shell. klsjldfkjk 
+ * Desktop Mode allows the user to experience the GNOME Shell. 
  * 
  ********************************************************************************/
 
 
 function desktopMode() {
         document.getElementById('desktop_mode').addEventListener('click', function () {
-                var requestVolumeDown  = new XMLHttpRequest();                                                       
+                var requestVolumeDown  = new XMLHttpRequest();                                                      
                 requestVolumeDown.open('POST', '/desktop_mode');                         
                 requestVolumeDown.send();                                               
         });
