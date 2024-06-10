f6a2847a4f2646cdb07a32f929f32368a3695778
senalbulumulle <senal.bulumulle@gmail.com>
2024-06-10 19:02:56 -0400

    cleanup

--- Scripts/development-environment-setup-uninstall.sh
+++
@@ -1,7 +0,0 @@
-## development-environment-setup.sh
-## This script allows the ability to uninstall the development environment
-
-
-## The first thing to do is to update the packages
-
-sudo pacman -Syyu --noconfirm
