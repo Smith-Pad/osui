7a1c56cb7ec6a78dc7cea1973b99f75b264dc5ff
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-20 11:56:54 -0500

    change the index.css compile source to static/CSS

--- compile-ui.sh
+++ compile-ui.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Remove the old index.css file
-rm -f static/index.css
+rm -f static/CSS/index.css
 
 # Loop through all .scss files in the UI directory and append their contents to index.css
 for file in UI/*.scss
 do
-  sassc "$file" >> static/index.css
+  sassc "$file" >> static/CSS/index.css
 done
