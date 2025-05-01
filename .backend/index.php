05361aad94ad6d7ae22ddc0a45a0c91474360790
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-01 14:20:02 -0400

    include the [switch-to-malaysia-locales.php] file

--- .backend/index.php
+++ .backend/index.php
@@ -1,3 +1,4 @@
 <?php
 include "switch-to-srilanka-locales.php";                       // Include the switch-to-srilanka-locales.php file
+include "switch-to-malaysia-locales.php";                       // Include the switch-to-malaysia-locales.php file
 ?>
