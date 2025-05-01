322ee8fcb88d26cbc3b00997167f499bd3ad0b5e
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-01 14:43:25 -0400

    Include [reboot-system.php] file

--- .backend/index.php
+++ .backend/index.php
@@ -1,4 +1,5 @@
 <?php
 include "switch-to-srilanka-locales.php";                       // Include the switch-to-srilanka-locales.php file
 include "switch-to-malaysia-locales.php";                       // Include the switch-to-malaysia-locales.php file
+include "reboot-system.php";                                    // Include the reboot-system.php file
 ?>
