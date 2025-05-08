ba3b47ef62061603255f945675f3c2b3d0e43ddf
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-07 20:46:59 -0400

    Added [.backend/now-playing-media]

---
+++ .backend/now-playing-media/index.php
@@ -0,0 +1,12 @@
+<?php
+// now playing media
+
+// This backend allows the ability to fetch the current media playing
+// and display it on the frontend.
+
+
+system("playerctl metadata");
+
+
+
+?>
