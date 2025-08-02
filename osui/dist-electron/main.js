187cf61afdf70729ef810d1c76270d46139f7d8b
senalbulumulle <senal.bulumulle>
2025-08-02 07:47:18 -0400

    Formatted code 8 tabs

--- osui/dist-electron/main.js
+++ osui/dist-electron/main.js
@@ -11,35 +11,35 @@ const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
 let win;
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
-    webPreferences: {
-      preload: path.join(__dirname, "preload.mjs")
-    }
-  });
-  win.webContents.on("did-finish-load", () => {
-    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  });
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(path.join(RENDERER_DIST, "index.html"));
-  }
+        win = new BrowserWindow({
+                icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+                webPreferences: {
+                        preload: path.join(__dirname, "preload.mjs")
+                }
+        });
+        win.webContents.on("did-finish-load", () => {
+                win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+        });
+        if (VITE_DEV_SERVER_URL) {
+                win.loadURL(VITE_DEV_SERVER_URL);
+        } else {
+                win.loadFile(path.join(RENDERER_DIST, "index.html"));
+        }
 }
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    win = null;
-  }
+        if (process.platform !== "darwin") {
+                app.quit();
+                win = null;
+        }
 });
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+        if (BrowserWindow.getAllWindows().length === 0) {
+                createWindow();
+        }
 });
 app.whenReady().then(createWindow);
 export {
-  MAIN_DIST,
-  RENDERER_DIST,
-  VITE_DEV_SERVER_URL
+        MAIN_DIST,
+        RENDERER_DIST,
+        VITE_DEV_SERVER_URL
 };
