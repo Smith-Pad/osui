d1e47f46b7c9b59a0290d277a63b6cb7087c772b
senalbulumulle <senal.bulumulle>
2025-08-06 10:29:06 -0400

    Call [global-navigation-bar.jsx] in [App.jsx]

--- osui/src/App.jsx
+++ osui/src/App.jsx
@@ -12,8 +12,11 @@
 
 
 import React, { useState, useEffect } from 'react';
-
+import { Button } from "@fluentui/react";
 import Scroll from './Scroll.jsx'
+import GlobalNavigationBar from './global-navigation-bar.jsx'
+import './senalui.css';
+
 
 function App() {
 
@@ -21,6 +24,7 @@ function App() {
                 <>
                         <h1>OSUI</h1>
                         <Scroll />
+                        <GlobalNavigationBar />
                 </>
         );
 
