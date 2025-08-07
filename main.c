6d9135624bf9b124535ded07e89acbed3c3a8fc3
senalbulumulle <senal.bulumulle>
2025-08-07 11:35:54 -0400

    Updated main.c

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 
 int main() 
 {
