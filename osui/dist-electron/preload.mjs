9982641bc02a70c86259f920a02ea75c93b231ca
senalbulumulle <senal.bulumulle>
2025-08-09 07:56:39 -0400

    Removed

--- osui/dist-electron/preload.mjs
+++
@@ -1,1 +0,0 @@
-"use strict";const o=require("electron");o.contextBridge.exposeInMainWorld("ipcRenderer",{on(...e){const[n,r]=e;return o.ipcRenderer.on(n,(t,...c)=>r(t,...c))},off(...e){const[n,...r]=e;return o.ipcRenderer.off(n,...r)},send(...e){const[n,...r]=e;return o.ipcRenderer.send(n,...r)},invoke(...e){const[n,...r]=e;return o.ipcRenderer.invoke(n,...r)}});
